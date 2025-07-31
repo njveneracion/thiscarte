29e3ac209923e7c837a3fb338f078b3b30b67baf
Nelson Jay Veneracion <njsvenn@gmail.com>
2025-08-01 00:27:52 +0800

    project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>Welcome to the App</h1>
+      <p>This is a simple React application.</p>
+      <button onClick={() => alert("Button clicked!")}>Click Me</button>
+    </>
+  );
+}
+
+export default App;
